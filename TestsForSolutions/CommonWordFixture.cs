5794cbf35b628825362abe50e92dc83a31cc6289
Artem Pychenko <par@sitecore.net>
2020-02-18 22:42:33 +0800

    Added solution for Most Common Word problem

---
+++ TestsForSolutions/CommonWordFixture.cs
@@ -0,0 +1,29 @@
+﻿using EasyLevel.Strings.Solution.CommonWord;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions
+{
+    public class CommonWordFixture
+    {
+        [Theory]
+        [InlineData("Hello, hello, good morning; why good! Do you heLlo, you", new []{ "you", "good" }, "hello")]
+        [InlineData("Hello, hello, good morning; why good! Do you heLlo, you", new[] { "you" }, "hello")]
+        [InlineData("Hello, hello, good morning; why good! Do you heLlo, you good", new[] { "hello" }, "good")]
+        [InlineData("Hello, hello, good morning; why good! Do you heLlo, you", new[] { "hello", "good" }, "you")]
+        [InlineData("Hello, hello, good morning; why good! Do you do heLlo, you", new[] { "you", "good", "hello" }, "do")]
+        [InlineData("Hello", new string[] {} , "hello")]
+        [InlineData("q, r, t; t' r' q; t;", new [] {"q"} , "t")]
+        public void MostCommonWord(string paragraph, string[] banned, string mostCommonWord)
+        {
+            // Arrange
+            var commonWord = new CommonWord();
+
+            // Act
+            var result = commonWord.MostCommonWord(paragraph, banned);
+
+            // Assert
+            result.Should().Be(mostCommonWord);
+        }
+    }
+}
---
+++ src/Solution/CommonWord/CommonWord.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyLevel.Strings.Solution.CommonWord
+{
+    public class CommonWord
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            paragraph = paragraph.ToLower();
+            paragraph += ".";
+
+            var separators = new []
+            {
+                ' ', ',', '!', '?', ':', ';', '.', '\''
+            };
+            var wordsDict = new Dictionary<string, int>();
+
+            var sb = new StringBuilder();
+
+            foreach (var t in paragraph)
+            {
+                if (!separators.Contains(t))
+                {
+                    sb.Append(t);
+                }
+                else if (!banned.Contains(sb.ToString()) && wordsDict.ContainsKey(sb.ToString()) && sb.Length != 0)
+                {
+                    wordsDict[sb.ToString()] += 1;
+                    sb.Clear();
+                }
+                else if (!banned.Contains(sb.ToString()) && !wordsDict.ContainsKey(sb.ToString()) && sb.Length != 0)
+                {
+                    wordsDict.Add(sb.ToString(), 1);
+                    sb.Clear();
+                }
+                else if (banned.Contains(sb.ToString()) && sb.Length != 0)
+                {
+                    sb.Clear();
+                }
+            }
+
+            var keyWithBiggestNumber = wordsDict.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+
+            return keyWithBiggestNumber;
+        }
+    }
+}
