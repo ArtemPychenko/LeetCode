f4ab4c7fde857f588b0585ec6d28608b912124a1
Artem Pychenko <par@sitecore.net>
2020-02-22 17:34:41 +0800

    Added solution for "Two Sum Less Than K" problem with tests.

---
+++ TestsForSolutions/Arrays/SumLessThanKFixture.cs
@@ -0,0 +1,28 @@
+﻿using Arrays.Solution.SumLessThanK;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class SumLessThanKFixture
+    {
+        [Theory]
+        [InlineData(new[] {34, 23, 1, 24, 75, 33, 54, 8}, 60, 58)]
+        [InlineData(new[] {10, 20, 30}, 15, -1)]
+        [InlineData(new[] {10, 20, 30, 11, 22}, 33, 32)]
+        [InlineData(new int[] { }, 15, -1)]
+        [InlineData(new[] {10}, 15, -1)]
+        public void TwoSumLessThanK_WithValidData_ReturnsExpectedResult(int[] arr, int maximumNumber,
+            int expectedResult)
+        {
+            // Arrange
+            var sumLessThanK = new SumLessThanK();
+
+            // Act
+            var result = sumLessThanK.TwoSumLessThanK(arr, maximumNumber);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/SumLessThanK/SumLessThanK.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays.Solution.SumLessThanK
+{
+    public class SumLessThanK
+    {
+        public int TwoSumLessThanK(int[] arr, int k)
+        {
+            int maxToK = -1;
+
+            if (arr.Length < 2)
+            {
+                return maxToK;
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[i] + arr[j] < k && arr[i] + arr[j] > maxToK)
+                    {
+                        maxToK = arr[i] + arr[j];
+                    }
+                }
+            }
+
+            return maxToK;
+        }
+    }
+}
