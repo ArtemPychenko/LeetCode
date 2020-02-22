79b90e741147fc3c4e7e4dc2aa5f85060168396f
Artem Pychenko <par@sitecore.net>
2020-02-22 19:02:22 +0800

    Added solution for "Merge Sorted Array" problem with test.

---
+++ TestsForSolutions/Arrays/MergeSortedArraysFixture.cs
@@ -0,0 +1,24 @@
+﻿using Arrays.Solution.MergeSortedArrays;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class MergeSortedArraysFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3, 0, 0, 0}, 3, new[] {2, 5, 6}, 3, new[] {1, 2, 2, 3, 5, 6})]
+        public void Merge_WithValidData_ReturnsExpectedResult(int[] nums1, int m, int[] nums2, int n,
+            int[] expectedArray)
+        {
+            // Arrange
+            var mergeSortedArrays = new MergeSortedArrays();
+
+            // Act
+            var result = mergeSortedArrays.Merge(nums1, m, nums2, n);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/MergeSortedArrays/MergeSortedArrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arrays.Solution.MergeSortedArrays
+{
+    public class MergeSortedArrays
+    {
+        public int[] Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            Array.Copy(nums2, 0, nums1, m, n);
+
+            Array.Sort(nums1);
+
+            return nums1;
+        }
+    }
+}
