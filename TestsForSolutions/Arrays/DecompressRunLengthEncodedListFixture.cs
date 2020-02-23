f80e53971f41d9bf715e1e8c130b66b021516265
Artem Pychenko <par@sitecore.net>
2020-02-23 11:44:53 +0800

    Added solution for the "Decompress Run-Length Encoded List" problem with tests.

---
+++ TestsForSolutions/Arrays/DecompressRunLengthEncodedListFixture.cs
@@ -0,0 +1,25 @@
+﻿using Arrays.Solution.DecompressRunLengthEncodedList;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class DecompressRunLengthEncodedListFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3, 4}, new[] {2, 4, 4, 4})]
+        [InlineData(new[] {1, 2}, new[] {2})]
+        [InlineData(new[] {1, 2, 5, 42, 3, 8}, new[] {2, 42, 42, 42, 42, 42, 8, 8, 8})]
+        public void DecompressRLEList_WithValidData_ReturnsExpectedResult(int[] nums, int[] expectedArray)
+        {
+            // Arrange
+            var decompressRLEList = new DecompressRunLengthEncodedList();
+
+            // Act
+            var result = decompressRLEList.DecompressRLEList(nums);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/DecompressRunLengthEncodedList/DecompressRunLengthEncodedList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Arrays.Solution.DecompressRunLengthEncodedList
+{
+    public class DecompressRunLengthEncodedList
+    {
+        public int[] DecompressRLEList(int[] nums)
+        {
+            var decompressed = new List<int>();
+
+            for (int i = 0; i < nums.Length - 1; i += 2)
+            {
+                for (int k = 0; k < nums[i]; k++)
+                {
+                    decompressed.Add(nums[i + 1]);
+                }
+            }
+
+            return decompressed.ToArray();
+        }
+    }
+}
