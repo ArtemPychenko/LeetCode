bf62186ac4440759cd10c56594ecfa94ba70e0d8
Artem Pychenko <par@sitecore.net>
2020-02-24 06:24:34 +0800

    Added solution for "Pascal's Triangle" problem with test. Added statistics file.

---
+++ TestsForSolutions/Arrays/PascalsTriangleFixture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Arrays.Solution.PascalsTriangle;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class PascalsTriangleFixture
+    {
+        [Fact]
+        public void Generate_WithValidData_ReturnsExpectedResult()
+        {
+            // Arrange
+            var numRows = 5;
+            var expectedResult = new List<IList<int>>()
+            {
+                new List<int>()
+                {
+                    1
+                },
+                new List<int>()
+                {
+                    1, 1
+                },
+                new List<int>()
+                {
+                    1, 2, 1
+                },
+                new List<int>()
+                {
+                    1, 3, 3, 1
+                },
+                new List<int>()
+                {
+                    1, 4, 6, 4, 1
+                }
+            };
+            var pascalTriangle = new PascalsTriangle();
+
+            // Act
+            var result = pascalTriangle.Generate(numRows);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/PascalsTriangle/PascalsTriangle.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+
+namespace Arrays.Solution.PascalsTriangle
+{
+    public class PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                // first position in list will always be 1
+                result.Add(new List<int>() { 1 });
+
+                // starts at 1 because the calculated values start at index 1 in each list
+                for (int j = 1; j < i; j++)
+                    result[i].Add(result[i - 1][j - 1] + result[i - 1][j]);
+
+                // last position in every line is 1, except the first one
+                if (i > 0)
+                    result[i].Add(1);
+            }
+
+            return result;
+        }
+    }
+}
