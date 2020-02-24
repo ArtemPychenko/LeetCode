5839a7b814b95cb97e79b4c0d1886cece353d87a
Artem Pychenko <par@sitecore.net>
2020-02-24 22:13:10 +0800

    Added solution for the "High five" problem with test.

---
+++ TestsForSolutions/Arrays/HighestFiveFixture.cs
@@ -0,0 +1,44 @@
+﻿using Arrays.Solution.HighestFive;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class HighestFiveFixture
+    {
+        // Arrange
+        private int[][] _initialArray = new int[][]
+        {
+            new [] {1, 91},
+            new [] {1, 92},
+            new [] {2, 93},
+            new [] {2, 97},
+            new [] {1, 60},
+            new [] {2, 77},
+            new [] {1, 65},
+            new [] {1, 87},
+            new [] {1, 100},
+            new [] {2, 100},
+            new [] {2, 76}
+        };
+
+        private int[][] _expectedResult = new int[][]
+        {
+            new [] {1, 87},
+            new [] {2, 88}
+        };
+
+        [Fact]
+        public void HighFive_WithValidData_ReturnsExpectedResult()
+        {
+            // Arrange
+            var highestFive = new HighestFive();
+
+            // Act
+            var result = highestFive.HighFive(_initialArray);
+
+            // Assert
+            result.Should().BeEquivalentTo(_expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/HighestFive/HighestFive.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arrays.Solution.HighestFive
+{
+    public class HighestFive
+    {
+        private readonly Dictionary<int, List<int>> _idsWithGrades = new Dictionary<int, List<int>>();
+
+        public int[][] HighFive(int[][] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (_idsWithGrades.ContainsKey(items[i][0]))
+                {
+                    _idsWithGrades[items[i][0]].Add(items[i][1]);
+                }
+                else
+                {
+                    _idsWithGrades.Add(items[i][0], new List<int>() {items[i][1]});
+                }
+            }
+
+            var studentIds = _idsWithGrades.Keys.ToList();
+            studentIds.Sort();
+
+            int[][] arr = new int[studentIds.Count][];
+
+            for (int k = 0; k < studentIds.Count; k++)
+            {
+                var grades = _idsWithGrades[studentIds[k]];
+                grades.Sort();
+
+                var middle = 0;
+
+                for (int j = grades.Count - 1; j >= grades.Count - 5; j--)
+                {
+                    middle += grades[j];
+                }
+
+                arr[k] = new[] {studentIds[k], middle / 5};
+            }
+
+            return arr;
+        }
+    }
+}
