2592a06856f0f2c5d01aa55847d64d1f4851c9ff
Artem Pychenko <par@sitecore.net>
2020-02-24 11:05:30 +0800

    Added solution for the "Plus One" problem with tests. Updated statistics

---
+++ TestsForSolutions/Arrays/PlusOneFixture.cs
@@ -0,0 +1,26 @@
+﻿
+using Arrays.Solution.PlusOne;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class PlusOneFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3, 4}, new[] {1, 2, 3, 5})]
+        [InlineData(new[] {1, 2, 3, 9}, new[] {1, 2, 4, 0})]
+        [InlineData(new[] {9}, new[] {1, 0})]
+        public void AddOne_WithValidData_ReturnsExpectedResult(int[] digits, int[] expected)
+        {
+            // Arrange
+            var plusOne = new PlusOne();
+
+            // Act
+            var result = plusOne.AddOne(digits);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/PlusOne/PlusOne.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Arrays.Solution.PlusOne
+{
+    public class PlusOne
+    {
+        public int[] AddOne(int[] digits)
+        {
+            int index = digits.Length - 1;
+            while (index >= 0 && digits[index] == 9)
+            {
+                index--;
+            }
+
+            if (index < 0)
+            {
+                int[] res = new int[digits.Length + 1];
+                res[0] = 1;
+                return res;
+            }
+            else
+            {
+                digits[index++]++;
+                while (index < digits.Length)
+                {
+                    digits[index++] = 0;
+                }
+                return digits;
+            }
+        }
+    }
+}
