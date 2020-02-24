92ddd852dd792f3a098c81c6bf84613f5a944c79
Artem Pychenko <par@sitecore.net>
2020-02-25 07:31:30 +0800

    Added solution for the "Rotate array" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Arrays/RotateArrayFixture.cs
@@ -0,0 +1,28 @@
+﻿
+using Arrays.Solution.RotateArray;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class RotateArrayFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 4, 7, 9}, 4, new[] {2, 4, 7, 9, 1})]
+        [InlineData(new[] {1, 2, 45, -7, 11, 55, 74}, 2, new[] {55, 74, 1, 2, 45, -7, 11})]
+        [InlineData(new[] {1, 2, 4}, 4, new[] {4, 1, 2})]
+        [InlineData(new[] {1, 2, 4, 7, 9}, 0, new[] {1, 2, 4, 7, 9})]
+        [InlineData(new[] {1, 2, 3, 4, 5, 6, 7}, 3, new[] {5, 6, 7, 1, 2, 3, 4})]
+        public void Rotate_WithValidData_ReturnsExpectedResult(int[] nums, int k, int[] expectedResult)
+        {
+            // Arrange
+            var rotateArray = new RotateArray();
+
+            // Act
+            var result = rotateArray.Rotate(nums, k);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/RotateArray/RotateArray.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Arrays.Solution.RotateArray
+{
+    public class RotateArray
+    {
+        public int[] Rotate(int[] nums, int k)
+        {
+            int[] temp = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                temp[(i + k) % nums.Length] = nums[i];
+            }
+
+            return temp;
+        }
+    }
+}
