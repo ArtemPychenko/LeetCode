959520a51dfa00b37581f19850eb2856f453116a
Artem Pychenko <par@sitecore.net>
2020-02-26 06:07:02 +0800

    1. Added new project for Math problems 2. Added new area to test Math problems 3. Added solution for the "Subtract the Product and Sum of Digits of an Integer" problem 4. Updated statistics file

---
+++ TestsForSolutions/Math/SubtractionFixture.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Runtime.InteropServices.ComTypes;
+using FluentAssertions;
+using Math.Solution.Subtraction;
+using Xunit;
+
+namespace TestsForSolutions.Math
+{
+    public class SubtractionFixture
+    {
+        [Theory]
+        [InlineData(123, 0)]
+        [InlineData(74885, 8928)]
+        [InlineData(12, -1)]
+        [InlineData(0, 0)]
+        public void SubtractProductAndSum_WithValidData_ReturnsExpectedResult(int n, int expectedResult)
+        {
+            // Arrange
+            var subtract = new Subtraction();
+
+            // Act
+            var result = subtract.SubtractProductAndSum(n);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Math/Solution/Subtraction/Subtraction.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+
+namespace Math.Solution.Subtraction
+{
+    public class Subtraction
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            var digits = new List<int>();
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            while (n > 0)
+            {
+                digits.Add(n % 10);
+                n /= 10;
+            }
+
+            var sum = 0;
+            var prod = 1;
+
+            foreach (var digit in digits)
+            {
+                sum += digit;
+                prod *= digit;
+            }
+
+            return prod - sum;
+        }
+    }
+}
