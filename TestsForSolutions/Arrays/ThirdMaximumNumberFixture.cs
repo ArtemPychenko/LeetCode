8d306bcb6dcb65770998d3bcaece3d3175c9b91f
Artem Pychenko <par@sitecore.net>
2020-02-25 21:43:46 +0800

    Added solution for the "Third Maximum Number" problem with tests

---
+++ TestsForSolutions/Arrays/ThirdMaximumNumberFixture.cs
@@ -0,0 +1,31 @@
+﻿
+using Arrays.Solution.ThirdMaximumNumber;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class ThirdMaximumNumberFixture
+    {
+        [Theory]
+        [InlineData(new[] {3, 2, 7, 98, 5}, 5)]
+        [InlineData(new[] {3}, 3)]
+        [InlineData(new[] {3, 7}, 7)]
+        [InlineData(new[] {6, 6, 6, 6, 6}, 6)]
+        [InlineData(new[] {4, 90, 90, 90}, 90)]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        public void ThirdMax_WithValidData_ReturnsExpectedResult(int[] nums, int expectedResult)
+        {
+            // Arrange
+            var thirdMaxNumber = new ThirdMaximumNumber();
+
+            // Act
+            var result = thirdMaxNumber.ThirdMax(nums);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/ThirdMaximumNumber/ThirdMaximumNumber.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Arrays.Solution.ThirdMaximumNumber
+{
+    public class ThirdMaximumNumber
+    {
+        public int ThirdMax(int[] nums)
+        {
+            var counter = 1;
+            Array.Sort(nums);
+
+            if (nums.Length == 1 && nums.Any())
+            {
+                return nums[0];
+            }
+
+            if (nums.Length == 2 && nums.Any())
+            {
+                return nums[1];
+            }
+
+            if (!nums.Any())
+            {
+                return 0;
+            }
+
+            Array.Reverse(nums);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] != nums[i])
+                {
+                    counter++;
+                }
+
+                if (counter == 3)
+                {
+                    return nums[i];
+                }
+            }
+
+            if (counter == 2)
+            {
+                return nums[0];
+            }
+
+            if (counter == 1)
+            {
+                return nums[0];
+            }
+
+            return 0;
+        }
+    }
+}
