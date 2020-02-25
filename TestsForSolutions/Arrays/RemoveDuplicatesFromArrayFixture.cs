312b27869eb12792021c85f07b526a76b39ab628
Artem Pychenko <par@sitecore.net>
2020-02-25 15:39:08 +0800

    Added solution for the "Remove Duplicates from Sorted Array" problem with tests.

---
+++ TestsForSolutions/Arrays/RemoveDuplicatesFromArrayFixture.cs
@@ -0,0 +1,30 @@
+﻿
+using Arrays.Solution.RemoveDuplicatesFromArray;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class RemoveDuplicatesFromArrayFixture
+    {
+        [Theory]
+        [InlineData(new[] {0, 1, 1, 3, 6, 6, 7}, 5)]
+        [InlineData(new[] {0, 1}, 2)]
+        [InlineData(new[] {1, 1, 1, 3, 5, 5, 7, 7}, 4)]
+        [InlineData(new[] {0, 1, 1, 1, 1, 6, 6}, 3)]
+        [InlineData(new[] {1, 1, 1, 1, 1, 1, 1}, 1)]
+        [InlineData(new[] {1}, 1)]
+        [InlineData(new int[] {}, 0)]
+        public void RemoveDuplicates_WithValidData_ReturnsExpectedResult(int[] nums, int expectedResult)
+        {
+            // Arrange
+            var removeDuplicates = new RemoveDuplicatesFromArray();
+
+            // Act
+            var result = removeDuplicates.RemoveDuplicates(nums);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/RemoveDuplicatesFromArray/RemoveDuplicatesFromArray.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Arrays.Solution.RemoveDuplicatesFromArray
+{
+    public class RemoveDuplicatesFromArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var counter = 1;
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return 1;
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] != nums[i])
+                {
+                    nums[counter] = nums[i];
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
