f20bb2ca1f1faf7a04281984f1f5409b497e1dae
Artem Pychenko <par@sitecore.net>
2020-02-27 07:52:18 +0800

    Added solution for "Add digits" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Math/AddNumberDigitsFixture.cs
@@ -0,0 +1,27 @@
+﻿
+using FluentAssertions;
+using Math.Solution.AddNumberDigits;
+using Xunit;
+
+namespace TestsForSolutions.Math
+{
+    public class AddNumberDigitsFixture
+    {
+        [Theory]
+        [InlineData(754, 7)]
+        [InlineData(1154, 2)]
+        [InlineData(23114567, 2)]
+        [InlineData(75654, 9)]
+        public void AddDigits_WithValidData_ReturnsExpectedResult(int num, int expectedResult)
+        {
+            // Arrange
+            var addNumberDigits = new AddNumberDigits();
+
+            // Act
+            var result = addNumberDigits.AddDigits(num);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Math/Solution/AddNumberDigits/AddNumberDigits.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Math.Solution.AddNumberDigits
+{
+    public class AddNumberDigits
+    {
+        public int AddDigits(int num)
+        {
+            var total = 0;
+
+            while (num > 0)
+            {
+                total += num % 10;
+                num = num / 10;
+
+                if (num == 0 && total / 10 != 0)
+                {
+                    num = total;
+                    total = 0;
+                }
+            }
+
+            return total;
+        }
+    }
+}
