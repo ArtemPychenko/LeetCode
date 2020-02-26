f117cd0ed47caa9b8da019c1ddc3769b28ec0d14
Artem Pychenko <par@sitecore.net>
2020-02-27 06:59:08 +0800

    Added solution for the "Happy number" problem with test. Updated statistics.

---
+++ TestsForSolutions/Math/HappyNumberFixture.cs
@@ -0,0 +1,25 @@
+﻿
+using FluentAssertions;
+using Math.Solution.HappyNumber;
+using Xunit;
+
+namespace TestsForSolutions.Math
+{
+    public class HappyNumberFixture
+    {
+        [Theory]
+        [InlineData(19, true)]
+        [InlineData(116, false)]
+        public void IsHappy_WithValidData_ReturnsExpectedResult(int number, bool expectedResult)
+        {
+            // Arrange
+            var happyNumber = new HappyNumber();
+
+            // Act
+            var result = happyNumber.IsHappy(number);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Math/Solution/HappyNumber/HappyNumber.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+
+namespace Math.Solution.HappyNumber
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            var existing = new HashSet<int>();
+
+            while (n != 1 && !existing.Contains(n))
+            {
+                existing.Add(n);
+                n = GetSquareSum(n);
+            }
+
+            return n == 1;
+        }
+
+        private int GetSquareSum(int n)
+        {
+            int totalSum = 0;
+            while (n > 0)
+            {
+                int t = n % 10;
+                n = n / 10;
+                totalSum += t * t;
+            }
+
+            return totalSum;
+        }
+    }
+}
