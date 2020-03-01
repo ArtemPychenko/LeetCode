fd02e0bd06c7713405509b9f6cde9792f1dcb56c
Artem Pychenko <par@sitecore.net>
2020-03-01 19:57:53 +0800

    Added few projects with solutions - for the "Logger rate limiter" and "Merge two sorted lists" problems. Updated statistics.

---
+++ src/EasyLevel/HashTable/Solution/LoggerRateLimiter/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HashTable.Solution.LoggerRateLimiter
+{
+    public class Logger
+    {
+        private readonly Dictionary<string, int> _logDictionary;
+
+        /** Initialize your data structure here. */
+        public Logger()
+        {
+            _logDictionary = new Dictionary<string, int>();
+        }
+
+        /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+            If this method returns false, the message will not be printed.
+            The timestamp is in seconds granularity. */
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if (!_logDictionary.ContainsKey(message))
+            {
+                _logDictionary.Add(message, timestamp);
+                return true;
+            }
+
+            var previousTimestamp = _logDictionary[message];
+
+            if (timestamp - previousTimestamp >= 10)
+            {
+                _logDictionary[message] = timestamp;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/EasyLevel/LinkedLists/Solution/MergeSortedLists/MergeSortedLists.cs
@@ -0,0 +1,44 @@
+﻿
+namespace LinkedLists.Solution.MergeSortedLists
+{
+    public class MergeSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode prehead = new ListNode(-1);
+
+            ListNode prev = prehead;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.Value <= l2.Value)
+                {
+                    prev.Next = l1;
+                    l1 = l1.Next;
+                }
+                else
+                {
+                    prev.Next = l2;
+                    l2 = l2.Next;
+                }
+
+                prev = prev.Next;
+            }
+
+            prev.Next = l1 ?? l2;
+
+            return prehead.Next;
+        }
+    }
+
+    public class ListNode
+    {
+        public ListNode(int x)
+        {
+           Value = x;
+        }
+        public int Value { get; set; }
+
+        public ListNode Next { get; set; }
+    }
+}
