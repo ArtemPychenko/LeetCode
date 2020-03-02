9923979f5c60e96cf8d9be39dd420e8223411a1c
Artem Pychenko <par@sitecore.net>
2020-03-02 14:26:18 +0800

    Added solution for the "License Key Formatting" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Strings/LicenseKeyFormattingFixture.cs
@@ -0,0 +1,26 @@
+﻿
+using FluentAssertions;
+using Strings.Solution.LicenseKeyFormatting;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class LicenseKeyFormattingFixture
+    {
+        [Theory]
+        [InlineData("5F3Z-2e-9-w", 4, "5F3Z-2E9W")]
+        [InlineData("5F-3Z", 4, "5F3Z")]
+        [InlineData("2-5g-3-J", 2, "2-5G-3J")]
+        public void FormatLicenseKey_WithValidData_ReturnsExpectedResult(string s, int k, string expectedResult)
+        {
+            // Arrange
+            var licenseKeyFormatting = new LicenseKeyFormatting();
+
+            // Act
+            var result = licenseKeyFormatting.FormatLicenseKey(s, k);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/LicenseKeyFormatting/LicenseKeyFormatting.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections;
+using System.Text;
+
+namespace Strings.Solution.LicenseKeyFormatting
+{
+    public class LicenseKeyFormatting
+    {
+        public string FormatLicenseKey(string S, int K)
+        {
+            var sb = new StringBuilder();
+            var noDashes = new Stack();
+            var counter = 0;
+
+            foreach (var s in S)
+            {
+                if (s != '-')
+                {
+                    noDashes.Push(s);
+                }
+            }
+
+            foreach (var noDash in noDashes)
+            {
+                if (counter % K == 0 && counter != noDashes.Count && counter != 0)
+                {
+                    sb.Append("-");
+                }
+
+                sb.Append(noDash);
+                counter++;
+            }
+
+            char[] charArray = sb.ToString().ToCharArray();
+            Array.Reverse(charArray);
+
+            string final = new string(charArray);
+
+            return final.ToUpper();
+        }
+    }
+}
