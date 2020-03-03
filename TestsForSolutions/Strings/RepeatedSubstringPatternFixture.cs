4c7dc2382c22edcfe77c85489bb9599ed9cdb7a8
Artem Pychenko <par@sitecore.net>
2020-03-03 21:40:12 +0800

    Added solution for the "Repeated Substring Pattern" problem with tests. Statistics updated.

---
+++ TestsForSolutions/Strings/RepeatedSubstringPatternFixture.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Strings.Solution.RepeatedSubstringPattern;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class RepeatedSubstringPatternFixture
+    {
+        [Theory]
+        [InlineData("abcabcabcabcx", false)]
+        [InlineData("abcabcabcabcxx", false)]
+        [InlineData("abcabcabcabcxxx", false)]
+        [InlineData("abcabcabcabc", true)]
+        public void IsRepeatedSubstringPattern_WithValidDataProvided_ReturnsExpectedResult(string line,
+            bool expectedResult)
+        {
+            // Arrange
+            var repeatedSubstringPattern = new RepeatedSubstringPattern();
+
+            // Act
+            var result = repeatedSubstringPattern.IsRepeatedSubstringPattern(line);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/RepeatedSubstringPattern/RepeatedSubstringPattern.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Strings.Solution.RepeatedSubstringPattern
+{
+    public class RepeatedSubstringPattern
+    {
+        public bool IsRepeatedSubstringPattern(string s)
+        {
+            var n = s.Length;
+
+            for (int count = n / 2; count > 0; count--)
+            {
+                if (n % count != 0) continue;
+                var leftStr = s.Substring(0, count);
+
+                var isFinished = true;
+                for (int i = count; i < n; i += count)
+                {
+                    if (leftStr != s.Substring(i, count))
+                    {
+                        isFinished = false;
+                        break;
+                    }
+                }
+
+                if (isFinished) return true;
+            }
+
+            return false;
+        }
+    }
+}
