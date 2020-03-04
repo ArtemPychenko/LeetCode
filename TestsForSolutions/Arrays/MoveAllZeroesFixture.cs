73fbb4c6a9f639d2a9292b8aae2e6ecbc1a75568
Artem Pychenko <par@sitecore.net>
2020-03-05 07:46:56 +0800

    Added solution for the "Move Zeroes" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Arrays/MoveAllZeroesFixture.cs
@@ -0,0 +1,25 @@
+﻿using Arrays.Solution.MoveAllZeroes;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class MoveAllZeroesFixture
+    {
+        [Theory]
+        [InlineData(new[] {0, 1, 0, 3, 12}, new[] {1, 3, 12, 0, 0})]
+        [InlineData(new[] {0, 1, 0, 3, 12, 5, 6, 7, 0, 8}, new[] {1, 3, 12, 5, 6, 7, 8, 0, 0, 0})]
+        [InlineData(new[] {0, 1, 0}, new[] {1, 0, 0})]
+        public void MoveZeroes_WithValidData_ReturnsExpectedResult(int[] nums, int[] expectedResult)
+        {
+            // Arrange
+            var moveAllZeroes = new MoveAllZeroes();
+
+            // Act
+            var result = moveAllZeroes.MoveZeroes(nums);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/MoveAllZeroes/MoveAllZeroes.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Arrays.Solution.MoveAllZeroes
+{
+    public class MoveAllZeroes
+    {
+        public int[] MoveZeroes(int[] nums)
+        {
+            var pointer = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0 && i == pointer)
+                {
+                    pointer++;
+                }
+                else if (nums[i] != 0 && i != pointer)
+                {
+                    nums[pointer] = nums[i];
+                    nums[i] = 0;
+                    pointer++;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
