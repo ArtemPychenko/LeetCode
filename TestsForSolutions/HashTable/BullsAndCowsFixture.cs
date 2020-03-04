a8313d0f9535e193a64a8769c862bd6a24b13d31
Artem Pychenko <par@sitecore.net>
2020-03-05 06:55:49 +0800

    Added solution for the "Bulls and Cows" problem with tests. Updated statistics.

---
+++ TestsForSolutions/HashTable/BullsAndCowsFixture.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using HashTable.Solution.BullsAndCows;
+using Xunit;
+
+namespace TestsForSolutions.HashTable
+{
+    public class BullsAndCowsFixture
+    {
+        [Theory]
+        [InlineData("12345", "54321", "1A4B")]
+        [InlineData("1", "1", "1A0B")]
+        [InlineData("1", "0", "0A0B")]
+        [InlineData("1234", "1235", "3A0B")]
+        public void GetHint_WithValidData_ReturnsExpectedResult(string secret, string guess, string expectedResult)
+        {
+            // Arrange
+            var bullsAndCows = new BullsAndCows();
+
+            // Act
+            var result = bullsAndCows.GetHint(secret, guess);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/HashTable/Solution/BullsAndCows/BullsAndCows.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+
+namespace HashTable.Solution.BullsAndCows
+{
+    public class BullsAndCows
+    {
+        public string GetHint(string secret, string guess)
+        {
+            var firstSecret = new Dictionary<int, int>();
+            var guessed = new List<int>();
+            var counterA = 0;
+            var counterB = 0;
+
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] == guess[i])
+                {
+                    counterA += 1;
+                    guessed.Add(i);
+                    continue;
+                }
+
+                if (firstSecret.ContainsKey(secret[i]))
+                {
+                    firstSecret[secret[i]] += 1;
+                }
+                else
+                {
+                    firstSecret.Add(secret[i], 1);
+                }
+            }
+
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (firstSecret.ContainsKey(guess[i]) && firstSecret[guess[i]] > 0 && !guessed.Contains(i))
+                {
+                    firstSecret[guess[i]] -= 1;
+                    counterB += 1;
+                }
+            }
+
+            return counterA + "A" + counterB + "B";
+        }
+    }
+}
