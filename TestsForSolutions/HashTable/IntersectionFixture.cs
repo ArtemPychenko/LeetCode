84a9dbd447fa7588d73df3e06ffee86a5d55159c
Artem Pychenko <par@sitecore.net>
2020-03-06 07:33:57 +0800

    Added solution for the "Intersection of Two Arrays" problem with tests. Updated statistics.

---
+++ TestsForSolutions/HashTable/IntersectionFixture.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HashTable.Solution.IntersectionOfTwoArrays;
+using Xunit;
+
+namespace TestsForSolutions.HashTable
+{
+    public class IntersectionFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 2, 1}, new[] {2, 2}, new[] {2})]
+        [InlineData(new[] {4, 9, 5}, new[] {9, 4, 9, 8, 4}, new[] {9, 4})]
+        public void Intersection_WithValidData_ReturnsExpectedResult(int[] nums1, int[] nums2, int[] expected)
+        {
+            // Arrange
+            var intersection = new IntersectionOfTwoArrays();
+
+            // Act
+            var result = intersection.Intersection(nums1, nums2);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/HashTable/Solution/IntersectionOfTwoArrays/IntersectionOfTwoArrays.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashTable.Solution.IntersectionOfTwoArrays
+{
+    public class IntersectionOfTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var existing = new List<int>();
+
+            if (nums1.Length == 0 || nums2.Length == 0)
+            {
+                return existing.ToArray();
+            }
+
+            foreach (var num in nums1)
+            {
+                if (((IList) nums2).Contains(num) && !existing.Contains(num))
+                {
+                    existing.Add(num);
+                }
+            }
+
+            return existing.ToArray();
+        }
+    }
+}
