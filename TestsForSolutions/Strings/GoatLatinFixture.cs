34313102a452f8955330157fe6f4f4836ae48e8d
Artem Pychenko <par@sitecore.net>
2020-03-06 06:57:04 +0800

    Added solution for the "Goat Latin" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Strings/GoatLatinFixture.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Strings.Solution.GoatLatin;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class GoatLatinFixture
+    {
+        [Theory]
+        [InlineData("I am", "Imaa ammaaa")]
+        [InlineData("Hello World people", "elloHmaa orldWmaaa eoplepmaaaa")]
+        public void ToGoatString_WithValidData_ReturnsExpectedResult(string s, string expected)
+        {
+            // Arrange
+            var goatString = new GoalLatin();
+
+            // Act
+            var result = goatString.ToGoatLatin(s);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/GoatLatin/GoalLatin.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings.Solution.GoatLatin
+{
+    public class GoalLatin
+    {
+        public string ToGoatLatin(string s)
+        {
+            var vowels = new List<char>
+            {
+                'i', 'I', 'e', 'E', 'a', 'A', 'o', 'O', 'u', 'U'
+            };
+
+            var words = s.Split(" ");
+            var sb = new StringBuilder();
+
+            if (s.Length == 0)
+            {
+                return null;
+            }
+
+            for (int i = 1; i <= words.Length; i++)
+            {
+                if (vowels.Contains(words[i - 1][0]))
+                {
+                    sb.Append(words[i - 1]);
+                    sb.Append("ma");
+                }
+                else
+                {
+                    sb.Append(words[i - 1].Substring(1));
+                    sb.Append(words[i - 1][0]);
+                    sb.Append("ma");
+                }
+                var repeatedA = new string('a', i);
+                sb.Append(repeatedA);
+                sb.Append(" ");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
