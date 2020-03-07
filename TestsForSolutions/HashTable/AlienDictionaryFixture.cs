e730f8c94f373f83de4cbefb17a3d574778737c8
Artem Pychenko <par@sitecore.net>
2020-03-07 21:21:55 +0800

    Added solution for the "Verifying an Alien Dictionary" problem with tests. Updated statistics.

---
+++ TestsForSolutions/HashTable/AlienDictionaryFixture.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using HashTable.Solution.AlienDictionary;
+using Xunit;
+
+namespace TestsForSolutions.HashTable
+{
+    public class AlienDictionaryFixture
+    {
+        [Theory]
+        [InlineData(new[] {"hello", "leetcode"}, "hlabcdefgijkmnopqrstuvwxyz", true)]
+        public void IsAlienSorted_WithValidData_ReturnsExpectedResult(string[] words, string order, bool expected)
+        {
+            // Arrange
+            var alienDictionary = new AlienDictionary();
+
+            // Act
+            var result = alienDictionary.IsAlienSorted(words, order);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
