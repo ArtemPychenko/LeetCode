875f32af5ac256393f77307118a7febc1d414736
Artem Pychenko <par@sitecore.net>
2020-03-09 21:04:52 +0800

    Added solution for the "First Unique Character in a String" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Strings/FirstUniqueCharInAStringFixture.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Strings.Solution.FirstUniqueCharInAString;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class FirstUniqueCharInAStringFixture
+    {
+        [Theory]
+        [InlineData("llhhtteeee", -1)]
+        [InlineData("hello", 0)]
+        [InlineData("lolohot", 4)]
+        public void FirstUniqChar_WithValidData_ReturnsExpectedResult(string s, int expected)
+        {
+            // Arrange
+            var uniqueChar = new FirstUniqueCharInAString();
+
+            // Act
+            var result = uniqueChar.FirstUniqChar(s);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/FirstUniqueCharInAString/FirstUniqueCharInAString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings.Solution.FirstUniqueCharInAString
+{
+    public class FirstUniqueCharInAString
+    {
+        public int FirstUniqChar(string s)
+        {
+            var dict = new Dictionary<char, int>();
+            var stringKey = "";
+
+            foreach (char ch in s)
+            {
+                if (dict.ContainsKey(ch))
+                {
+                    dict[ch] += 1;
+                }
+                else
+                {
+                    dict.Add(ch, 1);
+                }
+            }
+
+            foreach (var strKey in dict.Keys)
+            {
+                if (dict[strKey] == 1)
+                {
+                    stringKey = strKey.ToString();
+                    break;
+                }
+            }
+
+            return stringKey.Equals("") ? -1 : s.IndexOf(stringKey, StringComparison.Ordinal);
+        }
+    }
+}
