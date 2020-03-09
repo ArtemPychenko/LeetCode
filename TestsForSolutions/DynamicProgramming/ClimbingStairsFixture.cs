23cc87ecee40ce0d998c5205a4ad94502f39a0d9
Artem Pychenko <par@sitecore.net>
2020-03-10 06:40:59 +0800

    Added solution for the "Climbing Stairs" problem with tests. Updated statistics.

---
+++ TestsForSolutions/DynamicProgramming/ClimbingStairsFixture.cs
@@ -0,0 +1,26 @@
+﻿using DynamicProgramming.Solution.ClimbingStairs;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.DynamicProgramming
+{
+    public class ClimbingStairsFixture
+    {
+        [Theory]
+        [InlineData(3, 3)]
+        [InlineData(4, 5)]
+        [InlineData(15, 987)]
+        [InlineData(10, 89)]
+        public void ClimbStairs_WithValidData_ReturnsExpectedResult(int n, int expected)
+        {
+            // Arrange
+            var climbing = new ClimbingStairs();
+
+            // Act
+            var result = climbing.ClimbStairs(n);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/DynamicProgramming/Solution/ClimbingStairs/ClimbingStairs.cs
@@ -0,0 +1,25 @@
+﻿namespace DynamicProgramming.Solution.ClimbingStairs
+{
+    public class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            var arr = new int[n + 1];
+
+            arr[1] = 1;
+            arr[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+
+            return arr[n];
+        }
+    }
+}
