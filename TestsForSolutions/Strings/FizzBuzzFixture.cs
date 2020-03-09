df8c36c4484667439c5448823e7a6c96e93d99e2
Artem Pychenko <par@sitecore.net>
2020-03-09 20:16:48 +0800

    Added solution for the "Fizz Buzz" problem with tests. Updated statistics.

---
+++ TestsForSolutions/Strings/FizzBuzzFixture.cs
@@ -0,0 +1,26 @@
+﻿
+using FluentAssertions;
+using Strings.Solution.FizzBuzz;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class FizzBuzzFixture
+    {
+        [Theory]
+        [InlineData(5, new[] {"1", "2", "Fizz", "4", "Buzz"})]
+        [InlineData(15,
+            new[] {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"})]
+        public void IsFizzBuzz_WithValidData_ReturnsExpectedResult(int n, string[] expectedResult)
+        {
+            // Arrange
+            var fizzBuzz = new FizzBuzz();
+
+            // Act
+            var result = fizzBuzz.IsFizzBuzz(n);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections.Generic;
+
+namespace Strings.Solution.FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public IList<string> IsFizzBuzz(int n)
+        {
+            var fizzBuzz = new List<string>();
+
+            if (n < 1)
+            {
+                return fizzBuzz;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    fizzBuzz.Add("FizzBuzz");
+                    continue;
+                }
+
+                if (i % 3 == 0)
+                {
+                    fizzBuzz.Add("Fizz");
+                    continue;
+                }
+
+                if (i % 5 == 0)
+                {
+                    fizzBuzz.Add("Buzz");
+                    continue;
+                }
+
+                fizzBuzz.Add((i).ToString());
+            }
+
+            return fizzBuzz;
+        }
+    }
+}
