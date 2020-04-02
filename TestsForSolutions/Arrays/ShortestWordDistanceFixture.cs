a1fe96fbd87615a2c128a1239115b21dab3a0498
Artem Pychenko <par@sitecore.net>
2020-04-02 20:43:57 +0800

    Added solution for the "Shortest Word Distance" with the tests. Updated statistics.

---
+++ TestsForSolutions/Arrays/ShortestWordDistanceFixture.cs
@@ -0,0 +1,25 @@
+﻿using Arrays.Solution.ShortestWordDistance;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class ShortestWordDistanceFixture
+    {
+        [Theory]
+        [InlineData(new[] {"practice", "makes", "perfect", "coding", "makes"}, "coding", "practice", 3)]
+        [InlineData(new[] {"practice", "makes", "perfect", "coding", "makes"}, "makes", "coding", 1)]
+        public void ShortestDistance_WithValidData_ReturnsExpected(string[] words, string word1, string word2,
+            int difference)
+        {
+            // Arrange
+            var distance = new ShortestWordDistance();
+
+            // Act
+            var result = distance.ShortestDistance(words, word1, word2);
+
+            // Assert
+            result.Should().Be(difference);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/ShortestWordDistance/ShortestWordDistance.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Arrays.Solution.ShortestWordDistance
+{
+    public class ShortestWordDistance
+    {
+        public int ShortestDistance(string[] words, string word1, string word2)
+        {
+            int difference = 0;
+            int firstEntry = 0;
+            string previousMatch = null;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Equals(word1) || words[i].Equals(word2))
+                {
+                    if (string.Equals(previousMatch, null))
+                    {
+                        firstEntry = i;
+                        previousMatch = words[i];
+                        continue;
+                    }
+
+                    if (string.Equals(words[i], previousMatch))
+                    {
+                        firstEntry = i;
+                    }
+                    else if (!string.Equals(words[i], previousMatch))
+                    {
+                        if (difference > (i - firstEntry) || difference == 0)
+                        {
+                            difference = i - firstEntry;
+                        }
+
+                        previousMatch = words[i];
+                        firstEntry = i;
+                    }
+                }
+            }
+
+            return difference;
+        }
+    }
+}
