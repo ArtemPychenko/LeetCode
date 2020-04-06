34aeaca779e83e930b8e3c8d5084a1d25837e7b9
Artem Pychenko <par@sitecore.net>
2020-04-06 20:36:53 +0800

    Added solution for the "Group Anagrams" problem with test. Updated statistics.

---
+++ TestsForSolutions/Arrays/GroupedAnagramsFixture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Arrays.Solution.GroupedAnagrams;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class GroupedAnagramsFixture
+    {
+        [Fact]
+        public void GroupAnagrams_WithValidData_ReturnsExpected()
+        {
+            // Arrange
+            var init = new [] {"ate", "","eat", "", ""};
+            var expected = new List<IList<string>>
+            {
+                new List<string>()
+                {
+                    "ate", "eat"
+                },
+                new List<string>()
+                {
+                    "", "", ""
+                }
+            };
+
+            var groupedAnagrams = new GroupedAnagrams();
+
+            // Act
+            var result = groupedAnagrams.GroupAnagrams(init);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/GroupedAnagrams/GroupedAnagrams.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Arrays.Solution.GroupedAnagrams
+{
+    public class GroupedAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var sorted = Sort(strs);
+            var dictionary = new Dictionary<string, IList<string>>();
+            IList<IList<string>> sortedList = new List<IList<string>>();
+
+            if (!strs.Any())
+            {
+                return null;
+            }
+
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                if (dictionary.ContainsKey(sorted[i]))
+                {
+                    dictionary[sorted[i]].Add(strs[i]);
+                }
+                else
+                {
+                    dictionary.Add(sorted[i], new List<string>() {strs[i]});
+                }
+            }
+
+            foreach (var dictionaryValue in dictionary.Values)
+            {
+                sortedList.Add(dictionaryValue);
+            }
+
+            return sortedList;
+        }
+
+        private string[] Sort(string[] strings)
+        {
+            string[] sorted = new string[strings.Length];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                sorted[i] = string.Concat(strings[i].OrderBy(x => x));
+            }
+
+            return sorted;
+        }
+    }
+}
