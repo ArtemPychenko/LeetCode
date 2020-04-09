1faecc9a0686b09b7832f72f67fd45fd51d573bc
Artem Pychenko <par@sitecore.net>
2020-04-09 15:47:51 +0800

    Added solution for the "Backspace String Compare" problem with tests and updated statistics.

---
+++ TestsForSolutions/Strings/BackspaceStringCompareFixture.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Strings.Solution.BackspaceStringCompare;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class BackspaceStringCompareFixture
+    {
+        [Theory]
+        [InlineData("ab#c", "ad#c", true)]
+        [InlineData("ab##", "c#d#", true)]
+        [InlineData("a##c", "#a#c", true)]
+        [InlineData("a#c", "b", false)]
+        public void BackspaceCompare_WithValidData_ReturnsExpected(string S, string T, bool expected)
+        {
+            // Arrange
+            var stringComparer = new BackspaceStringCompare();
+
+            // Act
+            var result = stringComparer.BackspaceCompare(S, T);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/BackspaceStringCompare/BackspaceStringCompare.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+
+namespace Strings.Solution.BackspaceStringCompare
+{
+    public class BackspaceStringCompare
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            string s = null;
+            string t = null;
+            int counter = 0;
+
+            if (S == null && T == null)
+            {
+                return true;
+            }
+
+            if (S == null || T == null)
+            {
+                return false;
+            }
+
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (S[i] == '#')
+                {
+                    counter++;
+                    continue;
+                }
+
+                if (S[i] != '#' && counter == 0)
+                {
+                    s += S[i];
+                }
+                else if (S[i] != '#' && counter > 0)
+                {
+                    counter--;
+                }
+            }
+
+            counter = 0;
+
+            for (int i = T.Length - 1; i >= 0; i--)
+            {
+                if (T[i] == '#')
+                {
+                    counter++;
+                    continue;
+                }
+
+                if (T[i] != '#' && counter == 0)
+                {
+                    t += T[i];
+                }
+                else if (T[i] != '#' && counter > 0)
+                {
+                    counter--;
+                }
+            }
+
+            if (s == null && t == null)
+            {
+                return true;
+            }
+
+            if (s == null || t == null)
+            {
+                return false;
+            }
+
+            s = string.Concat(s.OrderBy(x => x));
+            t = string.Concat(t.OrderBy(x => x));
+
+            return s.Equals(t);
+        }
+    }
+}
