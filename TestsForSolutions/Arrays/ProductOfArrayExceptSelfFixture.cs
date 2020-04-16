4ba3cfc5bd9aab90e877400245734c8e8b9bafcb
Artem Pychenko <par@sitecore.net>
2020-04-16 09:18:42 +0800

    Added solution for the "Product of Array Except Self" problem with tests and updated statistics.

---
+++ TestsForSolutions/Arrays/ProductOfArrayExceptSelfFixture.cs
@@ -0,0 +1,25 @@
+﻿using Arrays.Solution.ProductOfArrayExceptSelf;
+using FluentAssertions;
+using Xunit;
+
+namespace TestsForSolutions.Arrays
+{
+    public class ProductOfArrayExceptSelfFixture
+    {
+        [Theory]
+        [InlineData(new[] {1, 2, 3, 4}, new[] {24, 12, 8, 6})]
+        [InlineData(new[] {3, 6, 15, 4}, new[] {360, 180, 72, 270})]
+        [InlineData(new[] {3, 7}, new[] {7, 3})]
+        public void ProductExceptSelf_WithValidData_ReturnsExpected(int[] nums, int[] expected)
+        {
+            // Arrange
+            var product = new ProductOfArrayExceptSelf();
+
+            // Act
+            var result = product.ProductExceptSelf(nums);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Arrays/Solution/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Arrays.Solution.ProductOfArrayExceptSelf
+{
+    public class ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var before = new int[nums.Length];
+            var after = new int[nums.Length];
+
+            before[0] = 1;
+            after[after.Length - 1] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                before[i] = before[i - 1] * nums[i - 1];
+            }
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                after[i] = after[i + 1] * nums[i + 1];
+            }
+
+            for (int k = 0; k < before.Length; k++)
+            {
+                before[k] *= after[k];
+            }
+
+            return before;
+        }
+    }
+}
