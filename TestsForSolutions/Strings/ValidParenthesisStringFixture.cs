deba43a9546ecd5034389fecf2b446252e4ab718
Artem Pychenko <par@sitecore.net>
2020-04-17 09:01:33 +0800

    Added solution for the "Valid Parenthesis String" problem and tests. Updated statistics.

---
+++ TestsForSolutions/Strings/ValidParenthesisStringFixture.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Strings.Solution.ValidParenthesisString;
+using Xunit;
+
+namespace TestsForSolutions.Strings
+{
+    public class ValidParenthesisStringFixture
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("(*)", true)]
+        [InlineData("(*))", true)]
+        public void CheckValidString_WithValidData_ReturnsExpected(string s, bool expected)
+        {
+            // Arrange
+            var validParenthesis = new ValidParenthesisString();
+
+            // Act
+            var result = validParenthesis.CheckValidString(s);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/EasyLevel/Strings/Solution/ValidParenthesisString/ValidParenthesisString.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Strings.Solution.ValidParenthesisString
+{
+    public class ValidParenthesisString
+    {
+        public bool CheckValidString(string s)
+        {
+            int left = 0, right = 0;
+
+            foreach (var c in s)
+            {
+                left += c == '(' ? 1 : -1;
+                right += c != ')' ? 1 : -1;
+
+                if (right < 0)
+                {
+                    break;
+                }
+
+                left = Math.Max(left, 0);
+            }
+
+            return left == 0;
+        }
+    }
+}
